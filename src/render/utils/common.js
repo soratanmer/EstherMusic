02eab8bab7b19259372f5cbbd0820d27aad0a621
soratanmer <soratanmer@gmail.com>
2021-09-30 00:25:56 +0800

    update isTrackPlayable

--- src/render/utils/common.js
+++ src/render/utils/common.js
@@ -26,7 +26,7 @@ export function isTrackPlayable(track) {
     } else if (track.noCopyrightRcmd !== null && track.noCopyrightRcmd !== undefined) {
         result.playable = false
         result.reason = '无版权'
-    } else if (track.privilege?.st < 0 && isAccountLoggedIn()) {
+    } else if (track.st === -1 && track.privilege?.st === -200) {
         result.playable = false
         result.reason = '已下架'
     }
